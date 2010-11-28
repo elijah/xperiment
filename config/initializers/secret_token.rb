d51e1498ae9b687b9bd050ce49a74a8032c9a67c
Elijah Wright <elijah.wright@gmail.com>
2010-11-27 20:29:45 -0600

    rails skeleton, actually commit the files this time

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Xperiment::Application.config.secret_token = '4c49b3e55597605a25d0e109cfdb265f82fc52352dfe01f81540b826742ff03527e08d9b46e81ec3bce00aa68ecbe61cc736b67227da0a7c3358c130c9fd5a6e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Xperiment::Application.config.session_store :cookie_store, :key => '_xperiment_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Xperiment::Application.config.session_store :active_record_store
